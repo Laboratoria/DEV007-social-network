6ad155f9e66608b10a7fd30b6ac4bb1e6ac73005
Katherine Encalada <katherineencalada@Katherines-MacBook-Air.local>
2023-06-06 21:16:36 -0500

    primera parte main

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { Home } from './components/Home.js';
+import { Register } from './components/Register.js';
+import { Login } from './components/Login.js';
+
+const rootDiv = document.getElementById('root');
+
+const routes = { 
+  '/': Home,
+  '/register': Register,
+  '/login': Login,
+};
+
+const onNavigate = (pathname) => {
+    window.history.pushState(
+        {},
+        pathname,
+        window.location.origin + pathname,
+    );
+
+    while (rootDiv.firstChild) {
+        rootDiv.removeChild(rootDiv.firstChild);
+    }
+
+    rootDiv.appendChild(routes[pathname]());
+};
+
+const component = routes[window.location.pathname];
+
+window.onpopstate = () => {
+    rootDiv.appendChild(component());
+};
+
+rootDiv.appendChild(component());
