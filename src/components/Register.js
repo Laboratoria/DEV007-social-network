1e67560c54c192dcc1c72927b8a84659e1e018af
Katherine <ktik0402@gmail.com>
2023-06-09 13:31:55 -0500

    register resgistratebutton

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,8 +3,8 @@ export const Register = (onNavigate) => {
   const HomeDiv = document.createElement("div");
   HomeDiv.textContent = "Bienvenida al registro";
   const buttonHome = document.createElement("button");
-
-  buttonHome.textContent = "Regresar al Home";
+  buttonHome.classList.add('registrateButton');
+  buttonHome.textContent = "Regístrate";
 
   buttonHome.addEventListener("click", () => onNavigate('/'));
   HomeDiv.appendChild(buttonHome);
