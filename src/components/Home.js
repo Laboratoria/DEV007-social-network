01d22bfed7fa5a9e7dfd0f82e4cddee1101aca7d
Cris-hr <cjminisol@gmail.com>
2023-06-12 10:13:38 -0500

    merge 12-06/arreglos conflictos-home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-
 export const Home = (onNavigate) => {
   const HomeDiv = document.createElement('div');
   const buttonLogin = document.createElement('button');
@@ -11,20 +10,20 @@ export const Home = (onNavigate) => {
   buttonRegister.classList.add('btnRegister');
   buttonGoogle.textContent = 'Entrar con Google';
   buttonGoogle.classList.add('btnGoogle');
-    HomeDiv.appendChild(buttonLogin);
-    HomeDiv.appendChild(buttonRegister);
+
+  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+  buttonRegister.addEventListener('click', () => onNavigate('/register'));
 
   HomeDiv.innerHTML += `
   <div class="imgGoogle">
     <img src= "./imagenes/logo-google.png" class = "logoGoogle" alt= "logo">
     <img src= "./imagenes/logo5.png" class = "logoRed" alt= "logoRedSocial">
-
   </div>
 `;
 
   HomeDiv.appendChild(buttonLogin);
   HomeDiv.appendChild(buttonRegister);
   HomeDiv.appendChild(buttonGoogle);
-  
+
   return HomeDiv;
 };
