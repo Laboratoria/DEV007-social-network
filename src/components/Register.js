b678b1591ea2518d15d56889112b367033fde98c
Cris-hr <cjminisol@gmail.com>
2023-06-12 12:16:06 -0500

    formulario register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,37 @@
 export const Register = (onNavigate) => {
-  const HomeDiv = document.createElement("div");
-  HomeDiv.textContent = "Bienvenida al registro";
-  const buttonHome = document.createElement("button");
+  const HomeDiv = document.createElement('div');
+  HomeDiv.textContent = 'Hola PetLover! Ingrese su información a continuación para registrarse';
+  HomeDiv.classList.add('registerContainer');
 
-  buttonHome.textContent = "Regresar al Home";
+  const inputName = document.createElement('input');
+  inputName.setAttribute('type', 'text');
+  inputName.setAttribute('placeholder', 'Nombre y Apellidos');
 
-  buttonHome.addEventListener("click", () => onNavigate('/'));
-  HomeDiv.appendChild(buttonHome);
+  const inputMail = document.createElement('input');
+  inputMail.setAttribute('type', 'text');
+  inputMail.setAttribute('placeholder', 'e-mail');
+
+  const inputPassword = document.createElement('input');
+  inputPassword.setAttribute('type', 'password');
+  inputPassword.setAttribute('placeholder', 'Contraseña');
+
+  const buttonRegistro = document.createElement('button');
+  buttonRegistro.classList.add('registerButton2');
+  buttonRegistro.textContent = 'Registrarse';
+
+  buttonRegistro.addEventListener('click', () => onNavigate('/'));
+
+  const registerConCuenta = document.createElement('p');
+  registerConCuenta.classList.add('registerConCuenta');
+  registerConCuenta.innerHTML += `
+  ¿ya tienes cuenta? <a href="/login" class="linkConCuenta"> Inicia Sesión </a>
+`;
+
+  HomeDiv.appendChild(inputName);
+  HomeDiv.appendChild(inputMail);
+  HomeDiv.appendChild(inputPassword);
+  HomeDiv.appendChild(buttonRegistro);
+  HomeDiv.appendChild(registerConCuenta);
 
   return HomeDiv;
 };
